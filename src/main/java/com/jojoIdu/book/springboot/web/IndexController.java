d339a6e1ae2a815788cf1d72b15752c4e5a58603
sangn <jmkang1984@gmail.com>
2024-01-11 00:49:36 +0900

    변경

--- src/main/java/com/jojoIdu/book/springboot/web/IndexController.java
+++ src/main/java/com/jojoIdu/book/springboot/web/IndexController.java
@@ -1,8 +1,10 @@
 package com.jojoIdu.book.springboot.web;
 
 
+import com.jojoIdu.book.springboot.config.auth.dto.SessionUser;
 import com.jojoIdu.book.springboot.service.posts.PostsService;
 import com.jojoIdu.book.springboot.web.dto.PostsResponseDto;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,11 +12,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 /*
     @GetMapping("/")
     public String index(){
@@ -24,6 +29,10 @@ public class IndexController {
     @GetMapping("/")
     public String index(Model model){
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if(user != null){
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
     @GetMapping("/posts/save")
@@ -38,6 +47,8 @@ public class IndexController {
 
         return "posts-update";
     }
+
+
 
 
 }
